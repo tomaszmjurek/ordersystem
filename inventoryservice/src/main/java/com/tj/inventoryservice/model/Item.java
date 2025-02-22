a9fd7dd3a73ef32d81bcb9ce7905d93e2cff2cad
tomasz.jurek <tomasz.jurek@aptitudesoftware.com>
2025-02-22 18:22:31 +0100

    Implement Kafka Listener with Order type

---
+++ inventoryservice/src/main/java/com/tj/inventoryservice/model/Item.java
@@ -0,0 +1,9 @@
+package com.tj.inventoryservice.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+@JsonDeserialize
+public record Item(String productId, int quantity) {
+}
---
+++ inventoryservice/src/main/java/com/tj/inventoryservice/model/Order.java
@@ -0,0 +1,13 @@
+package com.tj.inventoryservice.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.List;
+import java.util.UUID;
+
+@JsonDeserialize
+@JsonSerialize
+public record Order(UUID orderId, String customerId, List<Item> items) {
+
+}
