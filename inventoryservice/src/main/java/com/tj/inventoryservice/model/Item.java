166023fab14f868bd92d296e81c968842b21caa6
tomasz.jurek <tomasz.jurek@aptitudesoftware.com>
2025-02-23 17:12:38 +0100

    Add validations

--- inventoryservice/src/main/java/com/tj/inventoryservice/model/Item.java
+++ inventoryservice/src/main/java/com/tj/inventoryservice/model/Item.java
@@ -1,7 +1,6 @@
 package com.tj.inventoryservice.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.util.UUID;
 
